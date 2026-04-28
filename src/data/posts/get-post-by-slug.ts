fe6640a6b50f77dc8a55e3d1f8f019781580affb
Alex Assis <alex_assis17@hotmail.com>
2026-04-27 21:20:52 -0300

    fix: corrige erro 400 no fetch de post por slug usando populate plano

--- src/data/posts/get-post-by-slug.ts
+++ src/data/posts/get-post-by-slug.ts
@@ -4,7 +4,8 @@ import { fetchJson } from '@/utils/fetch-json';
 
 export async function getPostBySlug(slug: string | string[]): Promise<Post[]> {
   const slugString = Array.isArray(slug) ? slug[0] : slug;
-  const url = `${POST_URL}?filters[slug][$eq]=${slugString}&populate[author]=*&populate[category]=*&populate[cover]=*`;
+
+  const url = `${POST_URL}?filters[slug][$eq]=${slugString}&populate=*`;
 
   const json = await fetchJson<{ data: Post[] }>(url);
 
