0dc6c456cd4cd780b4479876f2983d7afac6a030
Alex Assis <alex_assis17@hotmail.com>
2026-05-06 22:06:12 -0300

    feat: add loading state for dynamic post fallback

---
+++ src/app/post/[slug]/loading.tsx
@@ -0,0 +1,16 @@
+export default function Loading() {
+  return (
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '50vh',
+        fontSize: '1.5rem',
+        fontWeight: 'bold',
+      }}
+    >
+      <p>Página ainda carregando... por favor aguarde!</p>
+    </div>
+  );
+}
